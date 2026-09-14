93324e47d93ffd39238331fbd82cf42e166e0c55
Bharat Shishodia <bharatshishodia345@gmail.com>
2026-09-14 21:41:41 +0530

    feat(recursion): add factorial solution

---
+++ 09_Recursion/01_Factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int sum = fact(n - 1);
+
+    return sum * n;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int ans = fact(n);
+    cout << ans << endl;
+}
